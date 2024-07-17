c204ff8cf6194d0432551a91c4d9f0be36be171a
Illia Vasylevskyi <ineersa@gmail.com>
2024-07-17 11:54:16 -0600

    Added some basic tests

---
+++ tests/Feature/ConnectionsSetupTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Turso\Driver\Laravel\Tests\Feature;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Turso\Driver\Laravel\Database\LibSQLConnection;
+use Turso\Driver\Laravel\Database\LibSQLDatabase;
+
+
+class ConnectionsSetupTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        if (File::exists('tests/_files/test.db')) {
+            File::delete('tests/_files/test.db');
+        }
+        parent::tearDown();
+    }
+
+    public function testConnectionInMemory(): void
+    {
+        config()->set('database.default', 'libsql');
+        config()->set('database.connections.libsql', [
+            'driver' => 'libsql',
+            'url' => 'file::memory:', // Taken from README docs
+            'authToken' => '', // This should be defied even with memory, which is not right
+            'syncUrl' => '', // This should be defied even with memory, which is not right
+            'encryptionKey' => '', // This should be defied even with memory, which is not right
+            'remoteOnly' => false, // This should be defied even with memory, which is not right
+            'prefix' => '',
+            'database' => null, // doesn't matter actually, since we use sqlite
+        ]);
+        // Get the default connection
+        $connection = DB::connection('libsql');
+
+        // Assert that the connection is an instance of LibSQLConnection
+        $this->assertInstanceOf(LibSQLConnection::class, $connection);
+
+        // Get the PDO instance
+        $pdo = $connection->getPdo();
+
+        $this->assertInstanceOf(LibSQLDatabase::class, $pdo);
+        $this->assertEquals('local', $pdo->getDb()->mode);
+        $this->assertEquals('memory', $pdo->getConnectionMode());
+        // we get "local" since our path is file:/turso-driver-laravel/vendor/orchestra/testbench-core/laravel/database/:memory:
+        // which is also wrong since we want just to use memory according to README.md
+        $result = $connection->select('PRAGMA database_list');
+        $this->assertNotEmpty($result);
+        $result = $pdo->query('SELECT sqlite_version()');
+        $this->assertNotEmpty($result, 'Failed to query SQLite version');
+    }
+
+    public function testConnectionLocalFile(): void
+    {
+        config()->set('database.default', 'sqlite');
+        config()->set('database.connections.local_file', [
+            'driver' => 'libsql',
+            'url' => 'file:/tests/_files/test.db',
+            'authToken' => '',
+            'syncUrl' => '',
+            'syncInterval' => 5,
+            'readYourWrites' => true,
+            'encryptionKey' => '',
+            'remoteOnly' => false,
+            'prefix' => '',
+            'database' => null, // doesn't matter actually, since we use sqlite
+        ]);
+
+        $connection = DB::connection('local_file');
+
+        // Assert that the connection is an instance of LibSQLConnection
+        $this->assertInstanceOf(LibSQLConnection::class, $connection);
+
+        // Get the PDO instance
+        $pdo = $connection->getPdo();
+
+        $this->assertInstanceOf(LibSQLDatabase::class, $pdo);
+        $this->assertEquals('local', $pdo->getDb()->mode);
+        $this->assertEquals('local', $pdo->getConnectionMode());
+        $result = $connection->select('PRAGMA database_list');
+        $this->assertNotEmpty($result);
+        $result = $pdo->query('SELECT sqlite_version()');
+        $this->assertNotEmpty($result, 'Failed to query SQLite version');
+
+        $this->assertTrue(File::exists('tests/_files/test.db'), 'No file created or wrong path');
+    }
+}
