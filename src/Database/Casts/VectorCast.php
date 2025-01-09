f2d9b8a0d3a62796beb76fb284d7f1a0809678b2
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-10 03:16:48 +0700

    feat(vector): add vector data type support

---
+++ src/Database/Casts/VectorCast.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Turso\Driver\Laravel\Database\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Facades\DB;
+
+class VectorCast implements CastsAttributes
+{
+    public function set($model, $key, $value, $attributes)
+    {
+        return DB::raw("vector32('[" . implode(',', $value) . "]')");
+    }
+
+    public function get($model, $key, $value, $attributes)
+    {
+        return json_decode($value, true);
+    }
+}
